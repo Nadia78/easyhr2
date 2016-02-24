f584999f7aa09931d305a00d172223033f6dddb8
SocAdam <iadjalian@gmail.com>
2016-02-24 18:03:51 +0100

    avancement registration and login

--- app/Controller/EntrepriseController.php
+++ app/Controller/EntrepriseController.php
@@ -14,4 +14,50 @@ class EntrepriseController extends Controller
 		$this->show('entreprise/index');
 	}
 
+	public function loginEntreprise() {
+
+		// affecter une variable à chaque valeur clé
+		$email=trim(htmlentities($_POST['email']));
+		$password=trim(htmlentities($_POST['password']));
+
+		// initialisation d'un tableau d'erreurs
+		$errors = [];
+		// Instanciation de UserManager
+		$entrepriseManager = new EntrepriseManager();
+		
+
+		$resultEntreprise = $entrepriseManager->getEntrepriseByUsernameOrEmail($email);
+
+		if($resultEntreprise){
+			//Instanciation d'un objet de type Authentification
+			$authentificationManager= new AuthentificationManager();
+			//
+			if ($authentificationManager->isValidLoginInfo($email, $password)) {
+				$authentificationManager->logUserIn($resultUser);
+				// Redirection
+				$this->redirectToRoute('privateHome');
+			}
+			else {
+				$errors['login'] = "Wrong password";
+			}
+
+		}
+		else {
+			$errors['login'] = "Cette adresse e-mail n'existe pas";
+		}
+		//
+		$this->show('login/index', ['errors' => $errors]);
+	}
+		public function logout()
+	{
+		//
+		$authentificationManager= new AuthentificationManager();
+		$authentificationManager->logUserOut($resultUser);
+
+		// redirection
+
+		$this->redirectToRoute('salarie');
+		
+	}
+
 }
--- app/Manager/EntrepriseManager.php
+++ app/Manager/EntrepriseManager.php
@@ -5,8 +5,44 @@ namespace Manager;
 
 /**
  * Classe requise par l'AuthentificationManager, éventuellement à extender par la UserManager de l'appli
- */
-class EntrepriseManager extends \W\Manager\Manager 
-{
+*/
+class EntrepriseManager extends \W\Manager\Manager {
+
+	public function emailExists($email){
+
+
+		$dbh = ConnectionManager::getDbh();
+		$sql = "SELECT email  FROM  entreprises WHERE email = :email LIMIT 1";
+		$sth = $dbh->prepare($sql);
+		$sth->bindValue(":email", $email);
+		if ($sth->execute()){
+			$foundUser = $sth->fetch();
+			if ($foundUser){
+				return true;
+			}
+		}
+
+		return false;
+		
+
+	}
 	
+	public function getEntrepriseByUsernameOrEmail($usernameOrEmail){
+
+
+		$dbh = ConnectionManager::getDbh();
+		$sql = "SELECT * FROM entreprises WHERE name = :username OR email = :email LIMIT 1";
+		$sth = $dbh->prepare($sql);
+		$sth->bindValue(":username", $usernameOrEmail);
+		$sth->bindValue(":email", $usernameOrEmail);
+		if ($sth->execute()){
+			$foundUser = $sth->fetch();
+			if ($foundUser){
+				return $foundUser;
+			}
+		}
+
+		return false;
+	}
+
 }
